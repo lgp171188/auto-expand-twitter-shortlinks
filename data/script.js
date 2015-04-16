d2a5356e08b0cedb82ab27624e8b6915def25d6e
L. Guruprasad <lgp171188@gmail.com>
2015-04-17 01:09:12 +0530

    Implement basic t.co links expansion in Twitter timeline

---
+++ data/script.js
@@ -0,0 +1,24 @@
+function replace_t_co_links() {
+    $('.twitter-timeline-link').each(function(){
+
+        if (!$(this).attr('data-url-expanded')) {
+        data_expanded_url = $(this).attr('data-expanded-url');
+
+        if (data_expanded_url) {
+        $(this).attr('href', data_expanded_url);
+        $(this).attr('data-url-expanded', '1');
+            $('> .js-display-url', this).html(data_expanded_url); }}
+    });
+}
+
+$(document).ready(function(){
+    replace_t_co_links();
+    console.error("Voila");
+    var observer = new MutationObserver(function(mutations){
+        replace_t_co_links();
+    });
+
+    var config = { attributes: true, childList: true, characterData: true, subtree: true };
+    observer.observe(document.body, config);
+
+});
